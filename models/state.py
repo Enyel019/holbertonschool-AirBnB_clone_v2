78b8013608d4440cc62b00eedc600dd2539eba02
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-05-25 20:18:58 -0400

    DBStorage - States and Cities

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""State Module for HBNB project."""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from models.city import City
+import models
+import os
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """State class."""
+
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='all, delete', backref='state')
+
+    if os.environ.get('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            final = []
+            city_list = models.storage.all(City)
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    final.append(city)
+            return final
