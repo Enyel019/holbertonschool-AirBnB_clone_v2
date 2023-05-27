d819f6e68b133d9bee327b4262dc2f32c7c10376
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-05-27 11:56:57 -0400

    Fix do_all

--- console.py
+++ console.py
@@ -13,6 +13,7 @@ from models.amenity import Amenity
 from models.review import Review
 from shlex import split
 import io
+import os
 
 
 class HBNBCommand(cmd.Cmd):
@@ -226,20 +227,24 @@ class HBNBCommand(cmd.Cmd):
     def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
         print_list = []
-
-        if args:
-            args = args.split(' ')[0]  # remove possible trailing args
-            if args not in HBNBCommand.classes:
-                print("** class doesn't exist **")
-                return
-            for k, v in storage._FileStorage__objects.items():
-                if k.split('.')[0] == args:
-                    print_list.append(str(v))
+        args = args.split(' ')[0]  # remove possible trailing args
+        
+        if os.environ.get ('HBNB_TYPE_STORAGE') == 'db':
+            pass
         else:
-            for k, v in storage._FileStorage__objects.items():
-                print_list.append(str(v))
+            if args:
+                
+                if args not in HBNBCommand.classes:
+                    print("** class doesn't exist **")
+                    return
+                for k, v in storage._FileStorage__objects.items():
+                    if k.split('.')[0] == args:
+                        print_list.append(str(v))
+            else:
+                for k, v in storage._FileStorage__objects.items():
+                    print_list.append(str(v))
 
-        print(print_list)
+            print(print_list)
 
     def help_all(self):
         """ Help information for the all command """
