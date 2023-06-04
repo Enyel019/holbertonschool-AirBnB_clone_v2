5be40bf856346c82156a43e4093fda71644797f4
Enyel019 <[el_dominic01@hotmail.com]>
2023-06-04 15:13:03 -0400

    1. HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application."""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """The function "hello" is defined but its implementation is missing."""
+    return "Hello HBNB!"
+
+@app.route("/", Strict_slashes=False)
+def hello_hbnb():
+    """The function "hello_hbnb()" is defined but has no code inside it."""
+    
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
