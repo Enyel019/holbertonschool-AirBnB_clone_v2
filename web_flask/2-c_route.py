88f61975ec557f78bf75b0e09fef4c3d89bc46da
Enyel019 <[el_dominic01@hotmail.com]>
2023-06-04 20:35:43 -0400

    new

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,27 +3,25 @@
 
 from flask import Flask
 
-
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello():
-    """The function "hello()" is defined but its implementation is missing."""
+    """Function "hello()" is defined but its implementation is missing."""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb",  strict_slashes=False)
 def hbnb():
-    """The function "hbnb()" is defined but has no code inside it."""
+    """Function "hbnb()" is defined but has no code inside it."""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def C_func(text):
-    """
-    The function "C_func" takes a text input."""
-    return 'C'.format(text.replace('_', ' '))
+def C_is_fun(text):
+    """Function "C_func" takes a text input."""
+    return f'C {text.replace("_", " ")}'
 
 
 if __name__ == '__main__':
