1fc0a259192e49366393ea73fe630d7c148d7fc4
Enyel019 <Enyelfm@gmail.com>
2023-06-10 13:40:25 -0400

    5. Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -40,5 +40,11 @@ def python_is_int(n):
     return f'{n} is a number'
 
 
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """Fifth route message."""
+    return render_template('5-number.html', n=n)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
