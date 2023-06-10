67bd9ed5f6b4a7cbf741ad33da1f3f27246ae583
Enyel019 <Enyelfm@gmail.com>
2023-06-10 14:16:08 -0400

    6. Odd or even?

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application."""
+
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """First route message."""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello_hbnb():
+    """Second route message."""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """Third route message."""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_is_cool(text='is cool'):
+    """Third route message."""
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>')
+def python_is_int(n):
+    """Fourth route message."""
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """Fifth route message."""
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def odd_even(n):
+    """Sixth route massage."""
+    return render_template('6-number_old_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
