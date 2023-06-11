5989344d270c73423c58fbd7d39a350e175ec243
Enyel019 <Enyelfm@gmail.com>
2023-06-10 23:59:54 -0400

    new

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone."""
+"""Is module defines a class to manage file storage for hbnb clone."""
 import json
-import contextlib
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format."""
+    """Is class manages storage of hbnb models in JSON format."""
 
     __file_path = 'file.json'
     __objects = {}
@@ -13,6 +12,8 @@ class FileStorage:
     def all(self, cls=None):
         """Return a dictionary or filtered list of models in storage."""
         if cls is None:
+            return FileStorage.__objects
+        else:
             filtered_objects = {}
             for key, value in FileStorage.__objects.items():
                 class_name = key.split('.')[0]
@@ -28,7 +29,7 @@ class FileStorage:
         """Save the storage dictionary to a file."""
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
-            temp |= FileStorage.__objects
+            temp.update(FileStorage.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f, indent=2)
@@ -48,20 +49,22 @@ class FileStorage:
             'State': State, 'City': City, 'Amenity': Amenity,
             'Review': Review
         }
-        with contextlib.suppress(FileNotFoundError):
+        try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
                     self.all()[key] = classes[val['__class__']](**val)
+        except FileNotFoundError:
+            pass
 
     def delete(self, obj=None):
         """Delete an object from the storage if it exists."""
         if obj is not None:
-            key = f'{obj.__class__.__name__}.{obj.id}'
+            key = obj.__class__.__name__ + '.' + obj.id
             if key in FileStorage.__objects:
                 del FileStorage.__objects[key]
 
     def close(self):
-        """Is function "close" is not defined and therefore."""
+        """Close the file storage and reload the data from JSON file."""
         self.reload()
