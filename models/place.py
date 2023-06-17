4d91a721ea6eaeb4c96532f222cb59a8711e1100
Enyel019 <el_dominic01@hotmail.com>
2023-06-17 15:17:39 -0400

    new code

--- models/place.py
+++ models/place.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
+"""Place Module for HBNB project."""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
