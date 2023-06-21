a02d5102c637939b0eabaf7118d18797f38d34d9
Enyel019 <el_dominic01@hotmail.com>
2023-06-20 22:47:52 -0400

    new

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""State Module for HBNB project."""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """The Amenity class inherits from the BaseModel and Base/
+    """The Amenity class inherits from the BaseModel and Base\
     classes in Python."""
 
     __tablename__ = 'amenities'
